2573b8464cbd0aada1ae4ccb5d1d856ac643e590
dsanders_development <dsanders@them0ve.com>
2017-07-28 17:57:16 +0000

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+https://preview.c9users.io/dsanders_m0veceo/coding-lessons/chrome-es2015/index.html?_c9_id=livepreview3&_c9_host=https://ide.c9.io 
+
+View application Preview .
--- js/main.es6.js
+++ js/main.es6.js
@@ -1,5 +1,5 @@
 //  app Functionality full
-
+'use strict';
 class FireNotesApp {
     
     // Initializes Fire Notes
@@ -26,6 +26,8 @@ class FireNotesApp {
     // Listen for updates on other windows
     window.addEventListener('storage', e => this.displayNote(e.key, e.newValue));
     }
+    
+    
         // Saves note to local storage 
     saveNote() {
         if (this.noteMessageInput.vale) {
@@ -54,7 +56,7 @@ class FireNotesApp {
         let note = document.getElementById(key);
         // If no element with key, create a new one 
         if (!note) {
-            note = document.createElement('sticky-note')
+            note = document.createElement('sticky-note');
             note.id = key;
             this.notesContainer.insertBefore(note, this.notesSectionTitle.nextSibling);
         }
@@ -69,50 +71,77 @@ class FireNotesApp {
     
     toggleButton() {
         if (this.noteMessageInput.value) {
-            this.addNoteButton.removeAttribute('disabled')
+            this.addNoteButton.removeAttribute('disabled');
         } else {
-            this.addNoteButton.setAttribute('disabled','true')
+            this.addNoteButton.setAttribute('disabled','true');
     }
    }
   }
 
 
+// on load and startup
+window.addEventListener('load', () => new FireNotesApp());
+
+
+// Sticky note custom element 
 class FireNote extends HTMLElement {
     
     // Fires when instance of the element is created 
     createdCallback() {
-        
+        this.classList.add(...FireNote.CLASSES);
+        this.innerHTML = FireNote.TEMPLATE;
+        this.messageElement = this.querySelector('.messsage');
+        this.dateElement = this.querySelector('.date');
+        this.deleteButton = this.querySelector('.delete');
+        this.deleteButton.addEventListener('click', () => this.deleteNote());
+
     }
     
     // Fires When an attribute of the element is added 
     attributeChangedCallback(attributeName) {
-        
+        // display/update the created date message if the id changes. 
+        if (attributeName == 'id') {
+            let date;
+            if (this.id) {
+                date - new Date(parseInt(this.id));
+            } else {
+                date = new Date();
+            }
+            
+            // Format Date 
+            
+            let dateFormatterOptions = {day: 'numeric', month: 'short'};
+            let shortDate = new intl.DateTimeFormat("en-US", dateFormatterOptions).format(date);
+            this.dateElement.textContent = `Created p ${shortDate}`;
+        }
     }
     
     // Sets Note Message
     setMessage(message) {
+        this.messageElement.textContent = message;
+        // Replace all line breaks
+        this.messageElement.innerHTML = this.messageElement.innerHTML.replace(/\n/g, '<br>');
         
     }
     
     // Deletes the note by removing the element
     deleteNote() {
-        
+        localStorage.removeItem(this.id);
+        this.parentNode.removeChild(this);
+        }
     }
     // Initial content of the element
-    FireNote.TEMPLATE = 
+    FireNote.TEMPLATE =  `
+    <div class="message"></div>
+    <div class="date"></div>
+    <button class="delete mdl-button mdl-js-button mdl-js-ripple-effect">
+      Delete
+    </button>`;
     
     // Fire Note elements top level style classes
-    FireNote.CLASSES = 
+    FireNote.CLASSES = ['mdl-cell--4-col-desktop', 'mdl-card__supporting-text', 'mdl-cell--12-col',
+  'mdl-shadow--2dp', 'mdl-cell--4-col-tablet', 'mdl-card', 'mdl-cell', 'sticky-note'];
     
-    // List of months (Shorthand)
-    
-    FireNote.MONTHS = 
-    
-}
-
-
-
-
 
 
 document.registerElement('fire-note', FireNote)
